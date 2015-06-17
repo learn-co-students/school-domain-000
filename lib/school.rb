dcb88f998a5574eccca6fe9427485fbf1f4e7a31
chaplonglau <chaplonglau@gmail.com>
2015-06-16 17:24:54 -0700

    sorted

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # Write code here
+
+class School
+
+  attr_accessor :school, :ROSTER
+
+  ROSTER={}
+
+  def initialize(school)
+    @school=school
+    ROSTER.clear
+  end
+
+  def roster
+    ROSTER
+  end
+
+  def add_student(name,grade)
+
+    if ROSTER.has_key?(grade)
+      ROSTER[grade] << "#{name}"
+    else
+      ROSTER[grade]= []
+      ROSTER[grade] << "#{name}"
+    end
+  end
+
+  def grade(num)
+    ROSTER[num]
+  end
+
+  def sort
+    ROSTER.each_value do |x|
+      x=x.sort!
+    end
+
+
+  end
+
+end
