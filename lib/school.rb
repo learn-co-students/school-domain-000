460b26cf95c72bcdef83464e66426be96e1b10ba
plai217 <philiplai217@gmail.com>
2015-06-24 11:34:51 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # Write code here
+
+class School
+
+attr_reader :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name,grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |x,y|
+      @roster[x].sort!
+    end
+    @roster
+  end
+
+
+end
+
