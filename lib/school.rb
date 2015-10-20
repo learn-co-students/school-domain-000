644f0937c150369be15c965ffa0385a30db770c1
NoneOfMaster <rcraigt@gmail.com>
2015-10-20 14:09:00 -0700

    funny school pun

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_reader :roster
+
+  def initialize(name) 
+    @roster = {}
+  end  
+
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else 
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each{|grade, names| 
+      sorted_roster[grade] = names.sort
+      }
+    sorted_roster
+  end
+
+end
